a028dbbfa7759e2db68de7042c0deca7f9d51266
Shahid N. Shah <shahid@shah.org>
2021-04-27 10:21:28 -0400

    fix: update to latest docopt.js

--- lib/deps.ts
+++ lib/deps.ts
@@ -3,9 +3,7 @@ export * as fs from "https://deno.land/std@0.90.0/fs/mod.ts";
 export * as fmt from "https://deno.land/std@0.90.0/fmt/printf.ts";
 export * as path from "https://deno.land/std@0.90.0/path/mod.ts";
 export * as uuid from "https://deno.land/std@0.90.0/uuid/mod.ts";
-//export * as docopt from "https://denopkg.com/Eyal-Shalev/docopt.js@v1.0.6/src/docopt.ts";
-// TODO: when Eyal-Shalev/docopt.js is upgraded, replace this with latest version:
-export * as docopt from "https://denopkg.com/netspective-studios/docopt-TEMP.js/src/docopt.ts";
+export * as docopt from "https://denopkg.com/Eyal-Shalev/docopt.js@v1.0.7/src/docopt.ts";
 export * as govnSvcVersion from "https://denopkg.com/gov-suite/governed-service-helpers@v0.3.15/version.ts";
 export * as textWhitespace from "https://denopkg.com/shah/text-whitespace@v1.0.4/mod.ts";
 export * as inflect from "https://denopkg.com/shah/text-inflect@v1.0.0/mod.ts";
